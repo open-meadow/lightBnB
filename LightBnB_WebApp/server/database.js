f3f5fbab82a4a70f5dd99f32a0cb353fab23d6b6
open-meadow <rohan.varughese@protonmail.com>
2022-12-15 23:42:56 +0000

    Refactor code by moving Pool into different file

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -1,14 +1,4 @@
-const properties = require('./json/properties.json');
-const users = require('./json/users.json');
-const { Pool } = require('pg');
-
-// connect to LightBnB database
-const pool = new Pool ({
-  user: 'vagrant',
-  password: '123',
-  host: 'localhost',
-  database: 'lightbnb'
-});
+const db = require('./db');
 
 /// Users
 
@@ -18,18 +8,7 @@ const pool = new Pool ({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  // let user;
-  // for (const userId in users) {
-  //   user = users[userId];
-  //   if (user.email.toLowerCase() === email.toLowerCase()) {
-  //     break;
-  //   } else {
-  //     user = null;
-  //   }
-  // }
-  // return Promise.resolve(user);
-
-  return pool
+  return db
   .query(`SELECT id, name, email, password FROM users WHERE email= $1`, [email])
   .then ((result) => {
     if(!result.rows[0]) {
@@ -52,7 +31,7 @@ exports.getUserWithEmail = getUserWithEmail;
 const getUserWithId = function(id) {
   // return Promise.resolve(users[id]);
 
-  return pool
+  return db
   .query(`SELECT id, name, email, password FROM users WHERE id= $1`, [id])
   .then ((result => {
     if(!result.rows[0]) {
@@ -79,7 +58,7 @@ const addUser =  function(user) {
   // users[userId] = user;
   // return Promise.resolve(user);
 
-  return pool
+  return db
   .query(`INSERT INTO users (name, email, password)
           VALUES ($1, $2, $3) RETURNING *`, [user.name, user.email, user.password])
   .then ((result) => {
@@ -99,7 +78,7 @@ exports.addUser = addUser;
 const getAllReservations = function(guest_id, limit = 10) {
   // return getAllProperties(null, 2);
 
-  return pool
+  return db
   .query(
     `SELECT properties.*, reservations.*
     FROM reservations
@@ -173,7 +152,7 @@ const getAllProperties = (options, limit = 10) => {
   `
   console.log(queryString, queryParams);
   
-  return pool
+  return db
     .query(queryString, queryParams)
     .then((result) => {
       console.log(result.rows);
@@ -240,7 +219,7 @@ const addProperty = function(property) {
 
   console.log("Query Params", queryParams);
 
-  return pool
+  return db
   .query(queryString, queryParams)
   .then((result) => {
     console.log(result.rows[0]);
@@ -274,7 +253,7 @@ const createReservation =  (reservation, guestId) => {
   console.log("Query string ", queryString);
   console.log("Query params ", queryParams);
 
-  return pool
+  return db
   .query(queryString, queryParams)
   .then((result) => {
     console.log(result.rows[0]);
